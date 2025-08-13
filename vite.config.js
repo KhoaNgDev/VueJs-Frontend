77cb41238234176ee71bd419e3391e328ea2d5a2
Nguyễn Khoa <134806583+ResumeKNTech@users.noreply.github.com>
2025-08-13 16:28:17 +0700

    s

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig(({ mode }) => {
   return {
-    base: './', 
+    base: '/', 
     plugins: [vue()],
     server: mode === 'development' ? {
       host: true,
