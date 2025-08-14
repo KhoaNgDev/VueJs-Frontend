74ac4e371e8f7eb2fcb70d31eb1e5a95fa8a7e1a
Nguyễn Khoa <134806583+ResumeKNTech@users.noreply.github.com>
2025-08-14 08:05:59 +0700

    clean

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,17 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-export default defineConfig(({ mode }) => {
-  return {
-    base: '/', 
-    plugins: [vue()],
-    server: mode === 'development' ? {
-      host: true,
-      port: 3000,
-      proxy: {
-        '/api': {
-          target: 'https://api.webnew.info.vn',
-          changeOrigin: true,
-        }
+export default defineConfig({
+  plugins: [vue()],
+  server: {
+    host: true,
+    port: 3000,
+    proxy: {
+      '/api': {
+        target: 'https://api.webnew.info.vn/',
+        changeOrigin: true,
       }
-    } : undefined,
-    build: {
-      outDir: 'dist',
-      emptyOutDir: true
     }
   }
 })
+
